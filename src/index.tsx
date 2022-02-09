6c0e60a4f63daab1fadd13200ed4c730940dab6b
João Pedro <jpmns9103@gmail.com>
2022-02-08 23:08:19 -0300

    criar estilo global da aplicação

--- src/index.tsx
+++ src/index.tsx
@@ -5,8 +5,11 @@ import App from './App';
 import { AuthProvider } from "./providers/AuthProvider";
 import { ModalProvider } from "./providers/ModalProvider";
 
+import GlobalStyle from "./styles/global"
+
 ReactDOM.render(
   <React.StrictMode>
+    <GlobalStyle />
     <AuthProvider>
       <ModalProvider>
         <App />
