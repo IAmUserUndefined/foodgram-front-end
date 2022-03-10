02e0bc47d7402da955983e1196ec81497dc037b5
João Pedro <jpmns9103@gmail.com>
2022-03-09 21:21:38 -0300

    início da refatoração do projeto para next

--- src/components/LoadingGif/index.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-import LoadigGifStyle from "./styles";
-
-const LoadingGif = () => {
-  return <LoadigGifStyle />;
-};
-
-export default LoadingGif;
--- src/styles/app.ts
+++
@@ -1,8 +0,0 @@
-import styled from "styled-components";
-
-const AppContainer = styled.div`
-    max-width: 1360px;
-    margin: 0 auto;
-`;
-
-export default AppContainer;
